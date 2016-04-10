ba69cd42a3d4e7881f1f5606ae1194a8aa511a26
fcs <fengchangsheng>
2016-04-10 21:55:15 +0800

    update

--- src/main/java/com/fcs/demo/collection/hashMap/SimpleHashMap.java
+++ src/main/java/com/fcs/demo/collection/hashMap/SimpleHashMap.java
@@ -1,5 +1,6 @@
 package com.fcs.demo.collection.hashMap;
 
+import javax.jws.Oneway;
 import java.util.*;
 
 /**
@@ -38,13 +39,54 @@ public class SimpleHashMap<K,V> extends AbstractMap<K,V> {
         return oldValue;
     }
 
-//    public V get(K k) {
-//
-//    }
+    public V get(Object key) {
+//        Iterator<Entry<K, V>> iterator = entrySet().iterator();
+//        while (iterator.hasNext()) {
+//            Entry<K, V>  entry = iterator.next();
+//            if (entry.getKey() == key) {
+//                return entry.getValue();
+//            }
+//        }
+//        return null;
+        /**
+         * 上面的写法可以根据key拿到value  但是建立在遍历所有的基础之上
+         */
+        int index = Math.abs(key.hashCode()) % size;
+        LinkedList<MapEntry<K, V>> bucket = buckets[index];
+        if (bucket == null) {
+            return null;
+        }else{
+            for (MapEntry<K, V> mapEntry : bucket) {
+                if (mapEntry.getKey().equals(key)) {
+                    return mapEntry.getValue();
+                }
+            }
+        }
+        return null;
+    }
 
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        return null;
+        Set<Entry<K, V>> set = new HashSet<Entry<K, V>>();
+        for (LinkedList<MapEntry<K, V>> linkedList : buckets) {
+            if (linkedList == null)
+                continue;
+            for (MapEntry<K, V> mapEntry : linkedList) {
+                set.add(mapEntry);
+            }
+        }
+        return set;
+    }
+
+    public static void main(String[] args) {
+        SimpleHashMap<String, String> map = new SimpleHashMap<String, String>();
+        map.put("1", "fcs1");
+        map.put("2", "fcs2");
+        map.put("3", "fcs3");
+        System.out.println(map);
+        System.out.println(map.get("3"));
+        System.out.println(map.size());
+
     }
 }
